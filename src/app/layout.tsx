5fc9348507219a4c63539535e510ff1c10ed9e95
Atakan Bayrak <atakan.bayrak@sekom.com.tr>
2026-01-20 15:03:31 +0300

    fix: layout fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import "../lib/styles/themes/default-theme.css";
 import type { ReactNode } from "react";
 import { EDKUIProvider } from "../lib";
 
@@ -8,13 +7,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="tr">
       <head>
         <title>EDK-UI</title>
-        <link rel="stylesheet" href="https://static.turkiye.gov.tr/themes/izmir/css/edkui-icon.css" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
-      </head>
+        </head>
       <body>
-        <EDKUIProvider>{children}</EDKUIProvider>
+        {children}
       </body>
     </html>
   );
