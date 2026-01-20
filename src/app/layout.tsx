6c8b030c93b50b3b96e560e1a9740755d74ad21d
Atakan Bayrak <atakan.bayrak@sekom.com.tr>
2026-01-20 14:57:55 +0300

    fix: first build commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import "../lib/styles/themes/default-theme.css";
+import type { ReactNode } from "react";
+import { EDKUIProvider } from "../lib";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="tr">
+      <head>
+        <title>EDK-UI</title>
+        <link rel="stylesheet" href="https://static.turkiye.gov.tr/themes/izmir/css/edkui-icon.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </head>
+      <body>
+        <EDKUIProvider>{children}</EDKUIProvider>
+      </body>
+    </html>
+  );
+}
