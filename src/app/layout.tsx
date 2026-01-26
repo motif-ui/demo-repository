bc893d68c4b793116998d89fa5e9904d02b94b6d
atakanbayrak55 <80352653+atakanbayrak55@users.noreply.github.com>
2026-01-26 12:03:28 +0300

    update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body>
+        <p>Unauth user PR from forked repository</p>
         <EDKUIProvider>{children}</EDKUIProvider>
       </body>
     </html>
